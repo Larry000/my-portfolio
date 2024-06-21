361e6c7af528c09677b51249e81c3287e4c9662e
Larry000 <159909962+Larry000@users.noreply.github.com>
2024-06-21 16:27:28 +0000

    Add files via upload

---
+++ larryking portifolio/king_s creative 2/jave.js
@@ -0,0 +1,33 @@
+// const name = "kofi"
+
+// const person = name >= "kofi" ? "true" : "false";
+
+// console.log(name);
+
+// const name= "kofi";
+
+// const person = ( name === "kofi")? "true" : "false";
+// console.log(name)
+
+// const names = [ "kofi", "Annie", "Eli","Larryking","Opoku" ];
+
+// const price = [ 20, 30, 40, 50, 60];
+
+// console.log AgeGroup = [ ...names, ...price]
+
+// console.log(AgeGroup);
+
+// const tickets ={
+//     types : "vip",
+//     quantity : 90,
+//     cost : "$600"
+// }
+// const events = { ...concerts , ...tickets };
+// console.log(events)
+
+//  this worked. this a templete l
+// const student = "larryking Israel Badu";
+
+// const studentfull = `full name is ${student}`
+
+// console.log(studentfull)
